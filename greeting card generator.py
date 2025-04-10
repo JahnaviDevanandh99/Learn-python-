6e193bf3e36fa567f50ec3afefc6a47b83c16c33
Jahnavi Devanandh <djahnavi985@gmail.com>
2025-04-10 21:22:32 +0530

    Add files via upload

---
+++ greeting card generator.py
@@ -0,0 +1,8 @@
+print("Welocome to Greeting Card Generator")
+your_name=input(" Enter Your Name ")
+guest_name=input(" Enter Your  guest's Name ")
+print(" I am " + your_name + " !")
+print("Hello " + guest_name + ", there is a small success party at my home, ")
+print( " you are warmly welcomed.  " )
+print(" THANK YOU ")
+
