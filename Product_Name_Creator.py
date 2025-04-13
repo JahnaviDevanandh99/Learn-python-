90d71d29e569846acee0ca6e5d02be37ee02a2e4
Jahnavi Devanandh <djahnavi985@gmail.com>
2025-04-13 14:35:53 +0530

    Add files via upload

---
+++ Product_Name_Creator.py
@@ -0,0 +1,10 @@
+# PRODUCT NAME CREATOR
+print("Welcome to the Product Name Creator")
+# TO ASK THE USER INPUT
+Brand_name = input("Enter your product name")
+Model_name = input("Enter your model name")
+#TO COMBINE INPUTS TO FORM PRODUCT NAME ANE DISPLAY
+print( "Product name :",Brand_name +'  '+Model_name)
+# TO FIND THE NUMBER OF CHARACTERS IN THE OUTPUT  
+print(len("product name"))
+
