f569e1f83c8964847b419397066eef8a821dc3b8
Jahnavi Devanandh <djahnavi985@gmail.com>
2025-04-15 01:08:14 +0530

    Add files via upload

---
+++ simple_addition_calculator.py
@@ -0,0 +1,14 @@
+# simple_addition_calculator
+print("welcome to the simple_addition_calculator")
+ # Ask for two numbers
+num1 = input("enter your first number:")
+num2 = input("enter your second number:")
+ #covert the input into numbers(float)
+num1 = float(num1)
+num2 = float(num2)
+ #add the two numbers
+result = num1 + num2
+ #print the results
+print("The result of adding {} and {} is:{}".format(num1,num2,result))
+
+
